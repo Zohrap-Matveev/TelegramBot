a5e2932ef2877faf52cdd56f26148b845f79aeac
Zohrap-Matveev <zohrap.matveev@gmail.com>
2024-03-01 19:06:01 +0400

    added button menu

--- src/main/java/am/matveev/TelegramBot/service/TelegramBot.java
+++ src/main/java/am/matveev/TelegramBot/service/TelegramBot.java
@@ -1,28 +1,53 @@
 package am.matveev.TelegramBot.service;
 
 import am.matveev.TelegramBot.config.BotConfig;
-import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
 public class TelegramBot extends TelegramLongPollingBot{
 
-    private final BotConfig botConfig;
+    final BotConfig botConfig;
+
+    static final String HELP_TEXT = "This bot is created to demonstrate Spring capabilities.\n\n" +
+            "You can execute commands from the main menu on the left or by typing a command:\n\n" +
+            "Type /start to see a welcome message\n\n" +
+            "Type /mydata to see data stored about yourself\n\n" +
+            "Type /help to see this message again";
+
+    public TelegramBot(BotConfig botConfig){
+        this.botConfig = botConfig;
+        List<BotCommand> listofCommands = new ArrayList<>();
+        listofCommands.add(new BotCommand("/start", "get a welcome message"));
+        listofCommands.add(new BotCommand("/mydata", "get your data stored"));
+        listofCommands.add(new BotCommand("/deletedata", "delete my data"));
+        listofCommands.add(new BotCommand("/help", "info how to use this bot"));
+        listofCommands.add(new BotCommand("/settings", "set your preferences"));
+        try{
+            this.execute(new SetMyCommands(listofCommands, new BotCommandScopeDefault(), null));
+        }catch(TelegramApiException e){
+            log.error("Error setting bot`s command list: " + e.getMessage());
+        }
+    }
 
     @Override
     public String getBotUsername(){
         return botConfig.getName();
     }
 
-    public String getBotToken() {
+    public String getBotToken(){
         return botConfig.getToken();
     }
 
@@ -30,36 +55,38 @@ public class TelegramBot extends TelegramLongPollingBot{
     @Override
     public void onUpdateReceived(Update update){
 
-        if(update.hasMessage() && update.getMessage().hasText()) {
+        if(update.hasMessage() && update.getMessage().hasText()){
             String messageText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
 
-            switch(messageText) {
-                case "/start" :
-                startCommandReceived(chatId ,update.getMessage().getChat().getFirstName());
-                break;
-
+            switch(messageText){
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                case "/help":
+                    sendMessage(chatId, HELP_TEXT);
+                    break;
                 default:
-                    sendMessage(chatId , "Sorry , but this command not recognized");
+                    sendMessage(chatId, "Sorry , but this command not recognized");
             }
         }
     }
 
-    private void startCommandReceived (long chatId,String name) throws TelegramApiException{
-         String answer = "Hi , " + name + " , nice to meet you!";
-         log.info("Replied to user " + name);
+    private void startCommandReceived(long chatId, String name) throws TelegramApiException{
+        String answer = "Hi , " + name + " , nice to meet you!";
+        log.info("Replied to user " + name);
 
-         sendMessage(chatId,answer);
+        sendMessage(chatId, answer);
     }
 
-    private void sendMessage(long chatId,String textToSend) throws TelegramApiException{
+    private void sendMessage(long chatId, String textToSend) throws TelegramApiException{
         SendMessage message = new SendMessage();
         message.setChatId(chatId);
         message.setText(textToSend);
 
-        try {
+        try{
             execute(message);
-        } catch(TelegramApiException e) {
+        }catch(TelegramApiException e){
             log.info("Error occurred: " + e.getMessage());
         }
 
